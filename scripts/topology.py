a5f5687518dc57c7c5c3c08fb858832fc4394991
Héctor Sabater López <hsabaterl3@gmail.com>
2025-10-13 19:41:14 +0200

    Add new network configuration

--- scripts/topology.py
+++ scripts/topology.py
@@ -1,84 +1,161 @@
 # Definición de redes
-redes = {
-    "red-servidor": {
-        "nombre": "red-servidor",
-        "bridge": "br-servidor",
-        "network": "10.100.0.0/24",
-        "gateway": "10.100.0.1",
-        "hosts": [
-            {"nombre": "servidor", "ip": "10.100.0.10", "mac": "52:54:00:10:00:01"},
-            {"nombre": "rc1-lan", "ip": "10.100.0.1", "mac": "52:54:00:10:00:02"}
-        ]
+config = {
+    "servidor": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-servidor",
+            "address": "10.100.0.10",
+            "netmask": "255.255.255.0",
+            "gateway": "10.100.0.1"
+            }
+        ],
+        "routing": []
     },
-    "red-troncal-rc1-rc2": {
-        "nombre": "red-troncal-rc1-rc2",
-        "bridge": "br-rc1-rc2",
-        "network": "10.1.0.0/30",
-        "gateway": "10.1.0.1",
-        "hosts": [
-            {"nombre": "rc1-wan", "ip": "10.1.0.1", "mac": "52:54:00:11:00:01"},
-            {"nombre": "rc2-wan", "ip": "10.1.0.2", "mac": "52:54:00:11:00:02"}
-        ]
+    "rc1": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.100.0.1",
+            "netmask": "255.255.255.252",
+            "gateway": "10.100.0.2"
+            },
+            {
+            "if": "eth1",
+            "bridge": "br-rc1-rc2",
+            "address": "10.1.0.1",
+            "netmask": "255.255.255.252",
+            "gateway": "10.1.0.2"
+            }
+        ],
+        "routing": []
     },
-    "red-troncal-rc2-ra": {
-        "nombre": "red-troncal-rc2-ra",
-        "bridge": "br-rc2-ra",
-        "network": "10.2.0.0/30",
-        "gateway": "10.2.0.1",
-        "hosts": [
-            {"nombre": "rc2-if-ra", "ip": "10.2.0.1", "mac": "52:54:00:12:00:01"},
-            {"nombre": "ra-wan", "ip": "10.2.0.2", "mac": "52:54:00:12:00:02"}
-        ]
+    "rc2": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.1.0.2",
+            "netmask": "255.255.255.252",
+            "gateway": "10.1.0.1"
+            }, 
+            {
+            "if": "eth1",
+            "bridge": "br-rc1-rc2",
+            "address": "10.2.0.1",
+            "netmask": "255.255.255.252",
+            "gateway": "10.1.0.1"
+            },
+            {
+            "if": "eth2",
+            "bridge": "br-rc1-rc2",
+            "address": "10.3.0.1",
+            "netmask": "255.255.255.252",
+            "gateway": "10.1.0.1"
+            }
+        ],
+        "routing": []
     },
-    "red-troncal-rc2-rb": {
-        "nombre": "red-troncal-rc2-rb",
-        "bridge": "br-rc2-rb",
-        "network": "10.3.0.0/30",
-        "gateway": "10.3.0.1",
-        "hosts": [
-            {"nombre": "rc2-if-rb", "ip": "10.3.0.1", "mac": "52:54:00:13:00:01"},
-            {"nombre": "rb-wan", "ip": "10.3.0.2", "mac": "52:54:00:13:00:02"}
-        ]
+    "ra": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.2.0.2",
+            "netmask": "255.255.255.252",
+            "gateway": "10.2.0.1"
+            }, 
+            {
+            "if": "eth1",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.1.1",
+            "netmask": "255.255.255.0",
+            "gateway": "10.2.0.1"
+            },
+            {
+            "if": "eth2",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.2.1",
+            "netmask": "255.255.255.0",
+            "gateway": "10.2.0.1"
+            }
+        ],
+        "routing": []
     },
-    "red1": {
-        "nombre": "red1",
-        "bridge": "br-red1",
-        "network": "10.0.1.0/24",
-        "gateway": "10.0.1.1",
-        "hosts": [
-            {"nombre": "ra-if-red1", "ip": "10.0.1.1", "mac": "52:54:00:01:01:01"},
-            {"nombre": "host1", "ip": "10.0.1.2", "mac": "52:54:00:01:01:02"}
-        ]
+    "rb": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.3.0.2",
+            "netmask": "255.255.255.252",
+            "gateway": "10.3.0.1"
+            }, 
+            {
+            "if": "eth1",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.3.1",
+            "netmask": "255.255.255.0",
+            "gateway": "10.3.0.1"
+            },
+            {
+            "if": "eth2",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.4.1",
+            "netmask": "255.255.255.0",
+            "gateway": "10.3.0.1"
+            }
+        ],
+        "routing": []
     },
-    "red2": {
-        "nombre": "red2",
-        "bridge": "br-red2",
-        "network": "10.0.2.0/24",
-        "gateway": "10.0.2.1",
-        "hosts": [
-            {"nombre": "ra-if-red2", "ip": "10.0.2.1", "mac": "52:54:00:02:02:01"},
-            {"nombre": "host2", "ip": "10.0.2.2", "mac": "52:54:00:02:02:02"}
-        ]
+    "host1": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.1.2",
+            "netmask": "255.255.255.0",
+            "gateway": "10.0.1.1"
+            }
+        ],
+        "routing": []
     },
-    "red3": {
-        "nombre": "red3",
-        "bridge": "br-red3",
-        "network": "10.0.3.0/24",
-        "gateway": "10.0.3.1",
-        "hosts": [
-            {"nombre": "rb-if-red3", "ip": "10.0.3.1", "mac": "52:54:00:03:03:01"},
-            {"nombre": "host3", "ip": "10.0.3.2", "mac": "52:54:00:03:03:02"}
-        ]
+    "host2": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.2.2",
+            "netmask": "255.255.255.0",
+            "gateway": "10.0.2.1"
+            }
+        ],
+        "routing": []
     },
-    "red4": {
-        "nombre": "red4",
-        "bridge": "br-red4",
-        "network": "10.0.4.0/24",
-        "gateway": "10.0.4.1",
-        "hosts": [
-            {"nombre": "rb-if-red4", "ip": "10.0.4.1", "mac": "52:54:00:04:04:01"},
-            {"nombre": "host4", "ip": "10.0.4.2", "mac": "52:54:00:04:04:02"}
-        ]
+    "host3": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.3.2",
+            "netmask": "255.255.255.0",
+            "gateway": "10.0.3.1"
+            }
+        ],
+        "routing": []
+    },
+    "host4": {
+        "ifs" : [
+            {
+            "if": "eth0",
+            "bridge": "br-rc1-rc2",
+            "address": "10.0.4.2",
+            "netmask": "255.255.255.0",
+            "gateway": "10.0.4.1"
+            }
+        ],
+        "routing": []
     }
 }
 
